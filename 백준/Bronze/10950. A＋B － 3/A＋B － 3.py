53aa5e5aed465a17e3ef71bf560b1fa32dd81254
gmk0603 <57435225+gmk0603@users.noreply.github.com>
2023-09-18 03:38:59 +0900

    [Bronze V] Title: A+B - 3, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,5 +1,12 @@
 T = int(input())
+a = []
+b = []
 for i in range(T):
     x, y = map(int, input().split())
-    print(x+y)
-
+    a.append(x)
+    b.append(y)
+for j in range(T):
+    if (a[j]=='' or b[j]==''):
+        continue
+    result = a[j] + b[j]
+    print(result)
